eefcd6ac2361687e6f0c4ed69700a61dea2c30a7
Fernando <pupygreen@gmail.com>
2022-12-13 16:18:23 -0300

    Profile-styles

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import React from 'react';
 import HeaderNoSearch from '../components/HeaderNoSearch';
 import Footer from '../components/Footer';
+import '../styles/Profile.css';
 
 function Profile() {
   const getEmail = localStorage
@@ -14,23 +15,41 @@ function Profile() {
   return (
     <>
       <HeaderNoSearch title="Profile" />
-      <span>E-mail: </span>
-      <p data-testid="profile-email">{getEmail}</p>
-      <Link to="/done-recipes">
-        <button type="button" data-testid="profile-done-btn">Done Recipes</button>
-      </Link>
-      <Link to="/favorite-recipes">
-        <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
-      </Link>
-      <Link to="/">
-        <button
-          type="button"
-          data-testid="profile-logout-btn"
-          onClick={ clearLocalStorage }
-        >
-          Logout
-        </button>
-      </Link>
+      <div className="info-user-profile">
+        <div className="user">
+          <span>E-mail: </span>
+          <p data-testid="profile-email">{getEmail}</p>
+        </div>
+        <div className="image-avatar">
+          <img
+            src="http://cdn.onlinewebfonts.com/svg/img_569204.png"
+            alt="avatar"
+            data-testid="profile-photo"
+          />
+        </div>
+        <div className="link-user-profile">
+          <Link to="/done-recipes">
+            <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+          </Link>
+          <Link to="/favorite-recipes">
+            <button
+              type="button"
+              data-testid="profile-favorite-btn"
+            >
+              Favorite Recipes
+            </button>
+          </Link>
+          <Link to="/">
+            <button
+              type="button"
+              data-testid="profile-logout-btn"
+              onClick={ clearLocalStorage }
+            >
+              Logout
+            </button>
+          </Link>
+        </div>
+      </div>
       <Footer />
     </>
   );
